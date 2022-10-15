90251019f9c49689aa7f57ffda41cab4945f4d03
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-10-14 23:19:38 -0500

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+}
---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import {Text, View} from "react-native"
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 120, color: "orange", textAlign: "center"}}>
+        Joynos inc.
+      </Text>
+    </View>
+  )
+}
+
+export default App
