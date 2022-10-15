a8e73c6c0ab05dcd5ce2ec80761b5b8850c4e6de
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-10-15 16:29:46 -0500

    search screen beginnings

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,28 @@
-import React from "react"
-import {Text, View} from "react-native"
+import React from 'react'
+import {Text, View} from 'react-native'
+import RestaurantCard from '../components/RestaurantCard'
+import SearchComponent from '../components/SearchComponent'
 
 const SearchScreen = () => {
   return (
-    <View>
-      <Text>Screen</Text>
+    <View style={{flex: 1, flexDirection: 'column', alignItems: 'flex-start'}}>
+      <SearchComponent />
+      <RestaurantCard
+        name="Mariscos Tenorio"
+        address="Av Pinar de la calma 777"
+        cost={3}
+        rating={4}
+        image=""
+        schedule="1:00 - 2:00"
+      />
+      <RestaurantCard
+        name="Mariscos Tenorio"
+        address="Av Pinar de la calma 777"
+        cost={3}
+        rating={4}
+        image=""
+        schedule="1:00 - 2:00"
+      />
     </View>
   )
 }
