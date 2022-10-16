83b7afddf505e8ffa63e5f165e76ee26342469e4
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-10-15 19:03:40 -0500

    resolved conflicts

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Text, View} from 'react-native'
+import FilterComponent from '../components/FilterComponent'
 
 const SearchScreen = () => {
   return (
     <View>
       <Text>Screen</Text>
+      <FilterComponent />
     </View>
   )
 }
