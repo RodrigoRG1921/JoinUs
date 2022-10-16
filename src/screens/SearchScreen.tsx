971ab4d2e59363c2153debf3746472986d069d6a
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-10-16 15:35:45 -0500

    removed text import

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {View} from 'react-native'
 import FilterComponent from '../components/FilterComponent'
 import SearchComponent from '../components/SearchComponent'
 
