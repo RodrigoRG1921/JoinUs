7885f4a9f0a7950d60b25fd7ec6a6682196b1f0e
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-10-16 20:51:21 -0500

    show restaurant cards

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,28 +1,44 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {ScrollView, ScrollViewComponent, View} from 'react-native'
 import RestaurantCard from '../components/RestaurantCard'
 import SearchComponent from '../components/SearchComponent'
+import restaurantsJSON from '../data/restaurants.json'
+
+interface Restaurant {
+  Nombre: string
+  Dirección: string
+  Imagen: string
+  Calificación: string
+  Horario: string
+  'Rango Precios': string
+  coordenadas?: string
+  listaTags?: string
+  Categoría: string
+}
 
 const SearchScreen = () => {
+  const restaurants: Restaurant[] = restaurantsJSON
   return (
-    <View style={{flex: 1, flexDirection: 'column', alignItems: 'flex-start'}}>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+      }}>
       <SearchComponent />
-      <RestaurantCard
-        name="Mariscos Tenorio"
-        address="Av Pinar de la calma 777"
-        cost={3}
-        rating={4}
-        image=""
-        schedule="1:00 - 2:00"
-      />
-      <RestaurantCard
-        name="Mariscos Tenorio"
-        address="Av Pinar de la calma 777"
-        cost={3}
-        rating={4}
-        image=""
-        schedule="1:00 - 2:00"
-      />
+      <ScrollView style={{flex: 1, width: '100%'}}>
+        {restaurants.map(restaurant => (
+          <RestaurantCard
+            key={restaurant.coordenadas}
+            name={restaurant.Nombre}
+            address={restaurant.Dirección}
+            cost={restaurant['Rango Precios']}
+            rating={Math.round(parseFloat(restaurant.Calificación))}
+            image={restaurant.Imagen}
+            schedule={restaurant.Horario}
+          />
+        ))}
+      </ScrollView>
     </View>
   )
 }
