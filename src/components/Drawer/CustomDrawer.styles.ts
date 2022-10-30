f3d6e1616ac3ea39a69cc5f756c20bbdabd81aa9
RodrigoRG1921 <rodrigo1921r@gmail.com>
2022-10-30 00:52:56 -0500

    finished custom drawer

---
+++ src/components/Drawer/CustomDrawer.styles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  titleBox: {margin: 20},
+  title: {
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  labelStyle: {color: 'black', fontSize: 20},
+})
+
+export {styles}
--- src/components/Drawer/CustomDrawer.tsx
+++ src/components/Drawer/CustomDrawer.tsx
@@ -1,21 +1,34 @@
 import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer'
 import React from 'react'
+import {View, Text} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import {styles} from './CustomDrawer.styles'
 
 const CustomDrawer = (props: any) => {
   return (
     <DrawerContentScrollView {...props}>
+      <View style={styles.titleBox}>
+        <Text style={styles.title}>JOIN US</Text>
+      </View>
       <DrawerItem
-        labelStyle={{color: '#609806'}}
+        style={{borderTopWidth: 1}}
+        labelStyle={styles.labelStyle}
         label="Busqueda"
         onPress={() => {
           props.navigation.navigate('SearchScreen')
         }}
+        icon={() => <Icon color={'black'} size={24} name="search" />}
       />
+
       <DrawerItem
         label="Busqueda avanzada"
+        labelStyle={styles.labelStyle}
         onPress={() => {
           props.navigation.navigate('StepperNavigator')
         }}
+        icon={({size}) => (
+          <Icon color={'black'} size={size} name="accessibility" />
+        )}
       />
     </DrawerContentScrollView>
   )
