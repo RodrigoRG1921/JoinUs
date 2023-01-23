9f94078f633bdedbb7f8c7088014ae6bfa262dfd
RodrigoRG1921 <rodrigo1921r@gmail.com>
2023-01-22 20:01:27 -0600

    added new card

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react'
-import {FlatList, Text} from 'react-native'
+import React, {useState, useCallback} from 'react'
 import FilterComponent from '../components/Filter/FilterComponent'
-import RestaurantCard from '../components/RestaurantCard'
 import SearchComponent from '../components/SearchComponent'
 import restaurantsJSON from '../data/restaurants.json'
 import ViewContainer from '../components/ViewContainer'
+import ResultsView from '../containers/ResultsView'
+import {View, Text} from 'react-native'
 
 interface Restaurant {
   name: string
@@ -18,95 +18,121 @@ interface Restaurant {
   category: string
 }
 
-const SearchScreen = () => {
+const SearchScreen = (props: any) => {
   const [filter, setFilter] = useState(false)
   const restaurants: Restaurant[] = restaurantsJSON
   const [currentRestaurants, setCurrentRestaurants] = useState(restaurants)
   const [currentFilters, setCurrentFilters] = useState<string[]>([])
   const [currentText, setCurrentText] = useState('')
 
-  const onChangeText = (text: string) => {
-    setCurrentText(text)
-    if (text === '') {
-      setCurrentRestaurants(restaurantsJSON)
-    } else {
-      const restaurantes = currentRestaurants.filter(restaurant => {
-        const name = restaurant.name.toLowerCase()
-        text = text.toLowerCase()
-        return name.includes(text)
-      })
-      setCurrentRestaurants(restaurantes)
-    }
-  }
-  const onCheckboxPress = (isChecked: boolean, filter: string) => {
-    if (isChecked) {
-      setCurrentFilters(prevFilters => [...prevFilters, filter])
-    } else {
-      setCurrentFilters(
-        currentFilters.filter(currentFilter => currentFilter !== filter),
-      )
-    }
-  }
+  const onChangeText = useCallback(
+    (text: string) => {
+      setCurrentText(text)
+      if (text === '') {
+        setCurrentRestaurants(restaurantsJSON)
+      } else {
+        const restaurantes = currentRestaurants.filter(restaurant => {
+          const name = restaurant.name.toLowerCase()
+          text = text.toLowerCase()
+          return name.includes(text)
+        })
+        setCurrentRestaurants(restaurantes)
+      }
+    },
+    [currentRestaurants],
+  )
+  const onCheckboxPress = useCallback(
+    (isChecked: boolean, filter: string) => {
+      if (isChecked) {
+        setCurrentFilters(prevFilters => [...prevFilters, filter])
+      } else {
+        setCurrentFilters(
+          currentFilters.filter(currentFilter => currentFilter !== filter),
+        )
+      }
+    },
+    [currentFilters],
+  )
 
-  const onFilterPress = () => {
+  const onFilterPress = useCallback(() => {
     setFilter(!filter)
     if (currentFilters.length > 0) {
-      const selectedRestaurants = currentRestaurants.filter(restaurant =>
+      const selectedFilteredRestaurants = restaurantsJSON.filter(restaurant =>
         currentFilters.every(filter =>
           restaurant.tags.split(', ').includes(filter),
         ),
       )
+      const selectedRestaurants = selectedFilteredRestaurants.filter(
+        restaurant => {
+          const name = restaurant.name.toLowerCase()
+          const text = currentText.toLowerCase()
+          return name.includes(text)
+        },
+      )
       setCurrentRestaurants(selectedRestaurants)
       setCurrentFilters([])
-    } else {
-      setCurrentRestaurants(restaurantsJSON)
     }
-  }
+  }, [filter, currentFilters])
 
-  const onSubmitText = () => {
-    setCurrentText('')
-  }
+  const onSubmitText = useCallback(() => {
+    const restaurantes = currentRestaurants.filter(restaurant => {
+      const name = restaurant.name.toLowerCase()
+      const text = currentText.toLowerCase()
+      return name.includes(text)
+    })
+    setCurrentRestaurants(restaurantes)
+  }, [currentText])
 
-  const onPressIn = () => {
+  const onPressIn = useCallback(() => {
     if (currentRestaurants.length === 0) {
       setCurrentRestaurants(restaurantsJSON)
     }
-  }
-  return (
-    <ViewContainer>
-      <SearchComponent
-        onFilterPress={onFilterPress}
-        onChangeText={onChangeText}
-        onSubmitText={onSubmitText}
-        currentText={currentText}
-        onPressIn={onPressIn}
+  }, [currentRestaurants])
+
+  const handleOnRestaurantClick = useCallback((restaurant: any) => {
+    props.navigation.navigate('RestaurantDetailScreen', {...restaurant})
+  }, [])
+
+  if (!filter && currentRestaurants.length > 0) {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{marginHorizontal: 16, marginVertical: 6}}>
+          <SearchComponent
+            onFilterPress={onFilterPress}
+            onChangeText={onChangeText}
+            onSubmitText={onSubmitText}
+            currentText={currentText}
+            onPressIn={onPressIn}
+          />
+        </View>
+        <ResultsView
+          onPress={handleOnRestaurantClick}
+          results={currentRestaurants}
+        />
+      </View>
+    )
+  } else if (filter) {
+    return (
+      <FilterComponent
+        onCheckboxPress={onCheckboxPress}
+        onPress={onFilterPress}
       />
-      {!filter && currentRestaurants.length > 0 ? (
-        <FlatList
-          keyExtractor={item => item.coords}
-          numColumns={1}
-          data={currentRestaurants}
-          renderItem={({item}) => (
-            <RestaurantCard
-              name={item.name}
-              address={item.address}
-              cost={item.priceRange}
-              rating={Math.round(parseFloat(item.rating))}
-              image={item.imageUri}
-              schedule={item.schedule}
-            />
-          )}
+    )
+  } else {
+    return (
+      <ViewContainer>
+        <SearchComponent
+          onFilterPress={onFilterPress}
+          onChangeText={onChangeText}
+          onSubmitText={onSubmitText}
+          currentText={currentText}
+          onPressIn={onPressIn}
         />
-      ) : filter ? (
-        <FilterComponent
-          onCheckboxPress={onCheckboxPress}
-          onPress={onFilterPress}
-        />
-      ) : (
+
         <Text>No restaurants with this characteristics </Text>
-      )}
-    </ViewContainer>
-  )
+      </ViewContainer>
+    )
+  }
 }
 
 export default SearchScreen
